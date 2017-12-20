67475b9b26b5dedafd06f973691a349383dd34a0
OPS—马 静 <jing.ma@quantgroup.cn>
2017-12-20 11:34:28 +0800

    change the function of check_data_consistency

--- airflow/utils/ali_odps.py
+++ airflow/utils/ali_odps.py
@@ -51,6 +51,7 @@ class MaxComputeAPI(ConnOdps):
             raise ValueError(u'table [%s] is not in project [%s]' % (src_table, self.project))
 
         filter_day = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y%m%d")
+        today = datetime.datetime.now() - datetime.timedelta(days=1)
 
         src_df = DataFrame(self.odps.get_table(src_table))
         dest_df = DataFrame(self.odps.get_table(dest_table))
@@ -58,13 +59,15 @@ class MaxComputeAPI(ConnOdps):
 
         dest_num_1 = dest_df.filter(dest_df.day == filter_day, dest_df.only != 1).revenue_prop.sum().execute()
         src_num_1 = src_df.left_join(join_df, on=[src_df.institution_id == join_df.i_id]).\
-            filter(src_df.end_time > datetime.datetime.now(), src_df.only != 1, join_df.i_institution_type_code == '0',
+            filter(src_df.end_time > datetime.datetime.now(), src_df.start_time < datetime.datetime(today.year, today.month, today.day),
+                   src_df.only != 1, join_df.i_institution_type_code == '0',
                    join_df.i_cooperate_state_code > 0).count().execute()
 
         dest_num_2 = dest_df.filter(dest_df.day == filter_day, dest_df.only == 1, dest_df.status == 1).revenue_prop.\
             sum().execute()
         src_num_2 = src_df.left_join(join_df, on=[src_df.institution_id == join_df.i_id]).\
-            filter(src_df.end_time > datetime.datetime.now(), src_df.only == 1, src_df.status == 1,
+            filter(src_df.end_time > datetime.datetime.now(), src_df.start_time < datetime.datetime(today.year, today.month, today.day),
+                   src_df.only == 1, src_df.status == 1,
                    join_df.i_institution_type_code == '0',
                    join_df.i_cooperate_state_code > 0).count().execute()
         # self.log.info("check_data_consistency==> dest_num_2:%s  src_num_2: %s" % (dest_num_2, src_num_2))
@@ -109,88 +112,88 @@ class MaxComputeAPI(ConnOdps):
     #             rest[col] = round((total_num - num)/float(total_num), 4)
     #     return rest
     #
-    # def _parse_partion(self, partion):
-    #     """
-    #     解析分区名字，得到分区日期
-    #     :param partion:
-    #     :return:
-    #     """
-    #     rest = {}
-    #     partion_day = None
-    #     if ',' in partion:
-    #         new = partion.split(',')
-    #     else:
-    #         new = [partion]
-    #     for item in new:
-    #         tmp = item.split('=')
-    #         rest[tmp[0]] = rest[tmp[1]]
-    #     if 'day' in rest:
-    #         day_time = rest.get('day')
-    #         if '_' in day_time:
-    #             elements = day_time.split('_')
-    #             partion_day = datetime.datetime(elements[0], elements[1], elements[2])
-    #         else:
-    #             partion_day = datetime.datetime(day_time[0:4], day_time[4:6], day_time[6:])
-    #     return partion_day
-    #
-    # def _get_histroy_inscrement_data(self, table):
-    #     """
-    #     获取历史增量
-    #     :param table:
-    #     :return:
-    #     """
-    #     global MIN_AND_MAX
-    #     histroy_day = datetime.datetime.today() - datetime.timedelta(days=1)
-    #     _table = self.odps.get_table(table)
-    #     for pt in _table.partitions:
-    #         pt_name = pt.name
-    #         partion_day = self._parse_partion(pt_name)
-    #         if partion_day < histroy_day:
-    #             with _table.open_reader(partition=pt_name) as reader:
-    #                 count = reader.count
-    #                 if MIN_AND_MAX:
-    #                     min_d = MIN_AND_MAX[0][1]
-    #                     max_d = MIN_AND_MAX[1][1]
-    #                     if count < min_d:
-    #                         MIN_AND_MAX[0] = (pt_name, count)
-    #
-    #                     if count > max_d:
-    #                         MIN_AND_MAX[1] = (pt_name, count)
-    #                 else:
-    #                     MIN_AND_MAX = [(pt_name, count), (pt_name, count)]
-    #
-    # def get_inscrement_data(self, table):
-    #     """
-    #     获取数据的增量
-    #     :return:
-    #     """
-    #     if not table:
-    #         raise ValueError(u'the param table is None')
-    #     if not self.odps.exist_table(table):
-    #         raise ValueError(u'table [%s] 不存在' % table)
-    #
-    #     self.log.info(u'get_inscrement_data==> MIN_AND_MAX: %s' % MIN_AND_MAX)
-    #     if not MIN_AND_MAX:
-    #         self._get_histroy_inscrement_data(table)
-    #
-    #     today = datetime.datetime.today() - datetime.timedelta(days=1)
-    #     _table = self.odps.get_table(table)
-    #
-    #     need_partions = [pt.name for pt in _table.partitions if self._parse_partion(pt.name) >= today]
-    #     self.log.info(u'get_inscrement_data==> need_partions: %s' % need_partions)
-    #     for pt in need_partions:
-    #         with _table.open_reader(pt) as reader:
-    #             count = reader.count
-    #             self.log.info("get_inscrement_data==> partion: %s num:%s " % (pt, count))
-    #             min_d = MIN_AND_MAX[0][1]
-    #             max_d = MIN_AND_MAX[1][1]
-    #             if count < min_d:
-    #                 MIN_AND_MAX[0] = (pt, count)
-    #
-    #             if count > max_d:
-    #                 MIN_AND_MAX[1] = (pt, count)
-    #
-    #     self.log.info(u'get_inscrement_data==> MIN_AND_MAX: %s' % MIN_AND_MAX)
+    def _parse_partion(self, partion):
+        """
+        解析分区名字，得到分区日期
+        :param partion:
+        :return:
+        """
+        rest = {}
+        partion_day = None
+        if ',' in partion:
+            new = partion.split(',')
+        else:
+            new = [partion]
+        for item in new:
+            tmp = item.split('=')
+            rest[tmp[0]] = rest[tmp[1]]
+        if 'day' in rest:
+            day_time = rest.get('day')
+            if '_' in day_time:
+                elements = day_time.split('_')
+                partion_day = datetime.datetime(elements[0], elements[1], elements[2])
+            else:
+                partion_day = datetime.datetime(day_time[0:4], day_time[4:6], day_time[6:])
+        return partion_day
+
+    def _get_histroy_inscrement_data(self, table):
+        """
+        获取历史增量
+        :param table:
+        :return:
+        """
+        global MIN_AND_MAX
+        histroy_day = datetime.datetime.today() - datetime.timedelta(days=1)
+        _table = self.odps.get_table(table)
+        for pt in _table.partitions:
+            pt_name = pt.name
+            partion_day = self._parse_partion(pt_name)
+            if partion_day < histroy_day:
+                with _table.open_reader(partition=pt_name) as reader:
+                    count = reader.count
+                    if MIN_AND_MAX:
+                        min_d = MIN_AND_MAX[0][1]
+                        max_d = MIN_AND_MAX[1][1]
+                        if count < min_d:
+                            MIN_AND_MAX[0] = (pt_name, count)
+
+                        if count > max_d:
+                            MIN_AND_MAX[1] = (pt_name, count)
+                    else:
+                        MIN_AND_MAX = [(pt_name, count), (pt_name, count)]
+
+    def get_inscrement_data(self, table):
+        """
+        获取数据的增量
+        :return:
+        """
+        if not table:
+            raise ValueError(u'the param table is None')
+        if not self.odps.exist_table(table):
+            raise ValueError(u'table [%s] 不存在' % table)
+
+        self.log.info(u'get_inscrement_data==> MIN_AND_MAX: %s' % MIN_AND_MAX)
+        if not MIN_AND_MAX:
+            self._get_histroy_inscrement_data(table)
+
+        today = datetime.datetime.today() - datetime.timedelta(days=1)
+        _table = self.odps.get_table(table)
+
+        need_partions = [pt.name for pt in _table.partitions if self._parse_partion(pt.name) >= today]
+        self.log.info(u'get_inscrement_data==> need_partions: %s' % need_partions)
+        for pt in need_partions:
+            with _table.open_reader(pt) as reader:
+                count = reader.count
+                self.log.info("get_inscrement_data==> partion: %s num:%s " % (pt, count))
+                min_d = MIN_AND_MAX[0][1]
+                max_d = MIN_AND_MAX[1][1]
+                if count < min_d:
+                    MIN_AND_MAX[0] = (pt, count)
+
+                if count > max_d:
+                    MIN_AND_MAX[1] = (pt, count)
+
+        self.log.info(u'get_inscrement_data==> MIN_AND_MAX: %s' % MIN_AND_MAX)
 
 if __name__ == '__main__':
     ACCESS_ID = ''
